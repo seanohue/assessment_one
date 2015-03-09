f646b13c79469c75d83966b7de8926cd0d8c44d2
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2015-03-09 09:50:07 -0400

    Added function to check isNaN, tested out okay.

--- sean_sortNumbers.js
+++ sean_sortNumbers.js
@@ -6,19 +6,24 @@ function sortNumbers(){
 }
 
 function checkIfNumber(input){
-
+	if (!isNaN(input))
+		return true;
+	else
+		return false;
 }
 
 function handleInput(){
 	for(var i=0; i<3; i++){
-		numbers[i] = sget("Enter number: ");
-		var isNumber = checkIfNumber(numbers[i]);
 		
+		numbers[i] = sget("Enter number: ").trim();
+		var isNumber = checkIfNumber(numbers[i]);
+		console.log(isNumber);
 		if (!isNumber){
 			i--;
 			console.log("Please input a number.");
 		}
 	}
+	
 	console.log(numbers); //for debugging
 }
 
