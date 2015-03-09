8135a0c7f7f7a0d844ab0f33d02d035efe93b070
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2015-03-09 09:46:34 -0400

    Added handleInput function. Not tested yet.

--- sean_sortNumbers.js
+++ sean_sortNumbers.js
@@ -5,16 +5,26 @@ function sortNumbers(){
 
 }
 
-function checkIfNumber(){
+function checkIfNumber(input){
 
 }
 
 function handleInput(){
-
+	for(var i=0; i<3; i++){
+		numbers[i] = sget("Enter number: ");
+		var isNumber = checkIfNumber(numbers[i]);
+		
+		if (!isNumber){
+			i--;
+			console.log("Please input a number.");
+		}
+	}
+	console.log(numbers); //for debugging
 }
 
 function displayNumbers(){
 
 }
 
+console.log("Enter three numbers and we will gladly sort them from largest to smallest!");
 handleInput();
