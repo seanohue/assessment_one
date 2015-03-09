370c48be781f3d5aa5528b62234c31c465f16987
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2015-03-09 10:35:20 -0400

    Added function to average, and flavored text to fit the weather theme.

--- sean_averageTemp.js
+++ sean_averageTemp.js
@@ -1,5 +1,4 @@
-
-
+var dailyTemps = new Array(7);
 
 function checkIfNumber(input){
 	if (!isNaN(input))
@@ -9,13 +8,25 @@ function checkIfNumber(input){
 }
 
 function handleInput(){
-	for(var i=0; i<7; i++){
+	for(var i=0; i<dailyTemps.length; i++){
 		
-		numbers[i] = sget("Enter number: ").trim();
-		var isNumber = checkIfNumber(numbers[i]);
+		dailyTemps[i] = sget("Enter the temperature for day "+i+" please: ").trim();
+		var isNumber = checkIfNumber(dailyTemps[i]);
 		console.log(isNumber);
 		if (!isNumber){
 			i--;
 			console.log("Please input a number.");
 		}
 	}
+
+function averageTemp(){
+	for (var i=0;i<dailyTemps.length; i++)
+		var total+=dailyTemps[i];
+	
+
+	return total/dailyTemps.length; 
+
+}
+
+console.log("Enter a number for the temperature of each day.");
+console.log("We will then find the average for the past week.");
