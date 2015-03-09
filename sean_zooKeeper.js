cf0d37e9ada13bb57299ba0ed75b17737dd1e2e5
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2015-03-09 10:56:19 -0400

    Added functions and sget.

---
+++ sean_zooKeeper.js
@@ -0,0 +1,15 @@
+var sget = require('sget');
+
+function nameAnimal(){}
+
+function mainMenu(){}
+
+function feedAnimal(){}
+
+function playWithAnimal(){}
+
+function trainAnimal(){}
+
+function showAnimal(){}
+
+function displayScore(){}
