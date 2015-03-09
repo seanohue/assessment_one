1e723c87ef4bfc328036ef6c8d89fc62b6c6318c
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2015-03-09 11:02:23 -0400

    Made a pretty sign.

--- sean_zooKeeper.js
+++ sean_zooKeeper.js
@@ -1,4 +1,9 @@
 var sget = require('sget');
+var name = null;
+var hunger = 100;
+var trained = 0;
+var score = 0;
+
 
 function nameAnimal(){}
 
@@ -13,3 +18,9 @@ function trainAnimal(){}
 function showAnimal(){}
 
 function displayScore(){}
+
+console.log("==============================");
+console.log("|    Welcome to GAMMA ZOO    |");
+console.log("|  Home of the Platypus Den  |");
+console.log("|Where YOU are the Zookeeper\u2122|")
+console.log("==============================");
