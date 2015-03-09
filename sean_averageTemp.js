0f23f6aaeccaf5f8cdc4458a097340e72d73ced7
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2015-03-09 10:43:32 -0400

    Found the bug. Also added a parseInt.

--- sean_averageTemp.js
+++ sean_averageTemp.js
@@ -1,3 +1,4 @@
+var sget = require('sget');
 var dailyTemps = new Array(7);
 
 function checkIfNumber(input){
@@ -10,7 +11,7 @@ function checkIfNumber(input){
 function handleInput(){
 	for(var i=0; i<dailyTemps.length; i++){
 		
-		dailyTemps[i] = sget("Enter the temperature for day "+i+" please: ").trim();
+		dailyTemps[i] = parseInt(sget("Enter the temperature for day "+(i+1)+" please: ").trim());
 		var isNumber = checkIfNumber(dailyTemps[i]);
 		//console.log(isNumber);
 
@@ -19,11 +20,13 @@ function handleInput(){
 			console.log("Please input a number.");
 		}
 	}
+}
 
 function averageTemp(){
 	var total = 0;
 	for (var i=0;i<dailyTemps.length; i++)
 		total+=dailyTemps[i];
+	console.log("Sum: "+total);
 	var average = total/(dailyTemps.length+1)
 	return average; 
 
