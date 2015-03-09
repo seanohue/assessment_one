b3e92ab2bc3e0339b81a246d733407b733d24218
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2015-03-09 11:13:43 -0400

    Made a menu and functions for input. Testing now...

--- sean_zooKeeper.js
+++ sean_zooKeeper.js
@@ -1,19 +1,59 @@
 var sget = require('sget');
 var name = null;
 var hunger = 100;
+var happiness = 50;
 var trained = 0;
 var score = 0;
+var turns = 1;
 
 
 function nameAnimal(){
 
-console.log("It is your first day on the job as an Apprentice Zookeeper.");
-console.log("You have been given a baby platypus to show your abilities.")
-console.log("What would you like to name your platypus?")
-name = sget("Please give it a cute name:").trim();
+	console.log("It is your first day on the job as an Apprentice Zookeeper.");
+	console.log("You have been given a baby platypus to show your abilities.")
+	console.log("What would you like to name your platypus?")
+	name = sget("Please give it a cute name:").trim();
+
 }
 
-function mainMenu(){}
+function mainMenu(){
+	
+	var validInput=false;
+	while (!validInput){
+	console.log("==============================");
+	console.log("It is day "+turns+" for you and your platypus, "+name+".");
+	console.log("What would you like to do?");
+	console.log("(Please enter a number...)");
+	console.log("==============================");
+	console.log("1) Feed "+name+" some delicious prawns.");
+	console.log("2) Play fetch with "+name+".");
+	console.log("3) Train "+name+" to do cool platypus tricks.");
+	console.log("4) Sign "+name+" up for the Greatest Platypus Show On Earth.");
+	console.log("==============================");
+
+	var choice = handleInput();
+	
+	}
+}
+
+function handleInput(){
+		choice = sget("Enter your choice...").trim();
+		var isNumber = checkIfNumber(numbers[i]);
+		//console.log(isNumber);
+		if (!isNumber){
+			console.log("Please input a number.");
+			handleInput();
+		}
+		else
+			return choice;
+	}
+
+function checkIfNumber(input){
+	if (!isNaN(input))
+		return true;
+	else
+		return false;
+}
 
 function feedAnimal(){}
 
