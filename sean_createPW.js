e348a59cb100c2da9353ff6a2e5b017d5708cead
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2015-03-09 10:28:10 -0400

    Added regex to check the password. Tested several times until it always worked.

--- sean_createPW.js
+++ sean_createPW.js
@@ -3,7 +3,7 @@ var password;
 
 function checkPasswordLength(){
 	
-	if (password.length >= 10){
+	if (password.length >= 11){
 		console.log("Your password fits our length requirements.");
 		return true;
 	}
@@ -14,11 +14,19 @@ function checkPasswordLength(){
 
 }
 
-
 function checkPasswordStrength(){
-	
+	var numbers = /\d+/;
+	var bang = /[!]+/;
 
+	if ((bang.test(password) === true) || (numbers.test(password) === true)){
+		console.log("Your password meets our strength requirements.");
+		return true;		
+	}
 
+	else{
+		console.log("Your password does not meet our strength requirements. Please use numbers or an exclamation point.");
+		return false;
+	}
 }
 
 function handleInput(){
@@ -26,11 +34,12 @@ function handleInput(){
 	var isLong = false;
 	var isStrong = false;
 
- 	while(!isStrong && !isLong){
+ 	while(!isStrong || !isLong){
 		password = sget("Enter your desired password: ");
 		isStrong = checkPasswordStrength();
 		isLong = checkPasswordLength();
 	}
+	console.log("Your password has been accepted! Enjoy your internetting.");
 }
 
 console.log("Welcome to www.internet.com!");
