0404316ccd8cbdd1dfab5fd158f71a208660cd27
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2015-03-09 09:41:02 -0400

    Made a template using functions.

---
+++ sean_sortNumbers.js
@@ -0,0 +1,20 @@
+var sget = require('sget');
+var numbers = new Array(3);
+
+function sortNumbers(){
+
+}
+
+function checkIfNumber(){
+
+}
+
+function handleInput(){
+
+}
+
+function displayNumbers(){
+
+}
+
+handleInput();
