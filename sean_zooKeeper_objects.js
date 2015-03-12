86841500af92c489f26924f207b1a52ec7d7ca44
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2015-03-12 11:18:23 -0400

    Implemented methods to change animal attributes/score. Fixed console.logs.

--- sean_zooKeeper_objects.js
+++ sean_zooKeeper_objects.js
@@ -5,14 +5,25 @@ Added animal object. Changed references to variables to match properties of obje
 Got rid of score.
 Created player object.
 Changed instances of 'Platypus' to 'animals.species'
+Added ability to look at the animal.
+Changed references to platypus to animal.species.
+Added player object with score, money, etc.
+
+3-11:
+Made a method for changing/incrementing attributes of animal. 
+
+3-12:
+Implemented semi-random changed to animal attributes & points.
+Changed multiple logs to linebreaks.
 
 TO DO
-Change multiple logs to linebreaks.
-Change platypus into animal object to allow for multiple animals. -- Sort of done.
-Change references to platypus to animal.species.
-Make a function(method, really) for changing/incrementing attributes of animal.
+Change display/train/feed/etc. functions so that they are part of the animal object
+	-- this will make multiple animals much easier to implement.
 Add ability to purchase additional animals.
-Add player object with animal inventory, score, money, etc.
+Add animal inventory to player, ability to switch between animals each day.
+Stuff like that.
+High score lists.
+Score multipliers for more dangerous/difficult animals.
 */
 
 var animal = {
@@ -22,7 +33,27 @@ var animal = {
 	happiness: 50,
 	training: 0,
 	cost: 100,
-	tricks:0
+	tricks:0,
+
+	changeHappiness: function(multiplier, modifier) {
+		this.happiness += (Math.floor(
+			Math.random() * multiplier) + modifier
+		)
+	},
+
+	changeHunger: function(multiplier, modifier) {
+		this.hunger += (Math.floor(
+			Math.random() * multiplier) + modifier
+		)
+	},
+
+	changeTraining: function(multiplier, modifier) {
+		this.training += (Math.floor(
+			Math.random() * multiplier) + modifier
+		)
+	}
+
+
 };
 
 var player = {
@@ -40,10 +71,10 @@ var turns = 1;
 
 function nameAnimal(){
 
-	console.log("It is your first day on the job as an Apprentice Zookeeper.");
-	console.log("You have been given a baby "+animal.species+" to show your abilities.");
-	console.log("What would you like to name your "+animal.species+"?");
+	console.log("It is your first day on the job as an Apprentice Zookeeper.\nYou have been given a baby "+animal.species+" to show your abilities.\nWhat would you like to name your "+animal.species+"?");
 	animal.name = sget("Please give it a cute name:").trim();
+	if (name=undefined)
+		nameAnimal();
 	clear();
 	namePlayer();
 }
@@ -51,6 +82,8 @@ function nameAnimal(){
 function namePlayer(){
 
 	player.name = sget("Please enter your name:").trim();
+	if (name=undefined)
+		namePlayer();
 	clear();
 }
 
@@ -59,21 +92,11 @@ function mainMenu(){
 	var endLoop=false;
 	while (!endLoop){
 		
-		console.log("==============================");
-		console.log("It is day #"+turns+" for you and your "+animal.species+", "+animal.name+".");
-		console.log("What would you like to do?");
-		console.log("(Please enter a number...)");
-		console.log("==============================");
-		console.log("1) Feed "+animal.name+" some delicious prawns.");
-		console.log("2) Play fetch with "+animal.name+".");
-		console.log("3) Train "+animal.name+" to do cool "+animal.species+" tricks.");
-		console.log("4) Sign "+animal.name+" up for the Greatest "+animal.species+" Show On Earth.");
-		console.log("5) Retire from zookeeping.");
-		console.log("==============================");
+		console.log("==============================\nIt is day #"+player.turns+" for you and your "+animal.species+", "+animal.name+".\nWhat would you like to do?\n(Please enter a number...)\n==============================)\n1) Feed "+animal.name+" some delicious prawns.\n2) Play fetch with "+animal.name+".\n3) Train "+animal.name+" to do cool "+animal.species+" tricks.\n4) Sign "+animal.name+" up for the Greatest "+animal.species+" Show On Earth.\n5) Check on "+animal.name+".\n6) Retire from zookeeping.\n==============================");
 
 		var choice = handleInput();
 		
-		if(choice > 0 && choice < 6){
+		if(choice > 0 && choice < 7){
 			goToChoice(choice);
 		}
 		else
@@ -119,6 +142,9 @@ function goToChoice(choice){
 			showAnimal();
 			break;
 		case 5:
+			displayAnimal();
+			break;
+		case 6:
 			retire();
 			break;
 		default:
@@ -136,24 +162,23 @@ function feedAnimal(){
 	console.log("You bring a bucket of tiny shrimp to "+animal.name+"'s habitat...");
 	if (animal.hunger>10){
 		console.log(animal.name+" seems pretty hungry... they chow down on the prawns and dance a delightful "+animal.species+" jig.");
-		animal.hunger-=20;
-		animal.happiness+=20;
-		animal.training-=1;
+		animal.changeHunger(-15,-10);
+		animal.changeTraining(-1,-1);
+		animal.changeHappiness(5,5);
 	}
 	else if (animal.hunger>0){
-		console.log(animal.name+" seems pretty content, but they eat a few prawns anyway... now they're stuffed!");
-		console.log(animal.name+" decides to take a nap...");
-		animal.hunger-=10;
-		animal.training-=5;
+		console.log(animal.name+" seems pretty content, but they eat a few prawns anyway... now they're stuffed!\n"+animal.name+" decides to take a nap...");
+		animal.changeHunger(-10,-5);
+		animal.changeTraining(-5,-1);
+		animal.changeHappiness(1,1)
 	}
 	else{
-		console.log(animal.name+" is stuffed and refuses to eat. You forcefeed them anyway, cruelly dumping prawns down their throat.");
-		console.log(animal.name+" looks sick and can hardly move. Poor thing!");
-		animal.hunger-=1;
-		animal.training-=5;
-		animal.happiness-=35;
+		console.log(animal.name+" is stuffed and refuses to eat. You forcefeed them anyway, cruelly dumping prawns down their throat.\n"+animal.name+" looks sick and can hardly move. Poor thing!");
+		animal.changeHunger(-2,-1);
+		animal.changeTraining(-5,-5);
+		animal.changeHappiness(-30,-15);
 	}
-	turns++;
+	player.turns++;
 	toContinue();
 }
 
@@ -162,20 +187,18 @@ function playWithAnimal(){
 	console.log("You toss a rubber ball over "+animal.name+"'s head...");
 
 	if (animal.hunger>40){
-		console.log(animal.name+" looks at the ball, then back at you.");
-		console.log(animal.name+" appears to be famished! No energy for playing fetch.");
-		animal.hunger+=10;
-		animal.happiness-=5;
-		animal.training-=5;
+		console.log(animal.name+" looks at the ball, then back at you.\n"+animal.name+" appears to be famished! No energy for playing fetch.");
+		animal.changeHunger(5,5);
+		animal.changeTraining(-5,-1);
+		animal.changeHappiness(-1,-5);
 	}
 	else {
-		console.log(animal.name+" gleefully fetches the ball for you over and over again.");
-		console.log(animal.name+" appears quite happy!");
-		animal.happiness+=20;
-		animal.hunger+=20;
-		animal.training+=5;
+		console.log(animal.name+" gleefully fetches the ball for you over and over again.\n"+animal.name+"  appears quite happy!");
+		animal.changeHunger(5,5);
+		animal.changeTraining(-1,-1);
+		animal.changeHappiness(20,15);
 	}
-	turns++;
+	player.turns++;
 	toContinue();
 }
 
@@ -184,34 +207,31 @@ function trainAnimal(){
 	console.log("You attempt to teach "+animal.name+" some new tricks...");
 
 	if (animal.hunger>40){
-		console.log(animal.name+" doesn't even seem to be paying attention.");
-		console.log(animal.name+" appears to be famished! No energy for learning.");
-		animal.hunger+=10;
-		animal.happiness-=5;
-		animal.training-=5;
+		console.log(animal.name+" doesn't even seem to be paying attention.\n"+animal.name+" appears to be famished! No energy for learning.");
+		animal.changeHunger(5,5);
+		animal.changeTraining(-5,-1);
+		animal.changeHappiness(-1,-5);
 	}
 	else if (animal.hunger<5){
-		console.log(animal.name+" decides to take a nap.");
-		console.log(animal.name+" is so stuffed they can barely move! No energy for learning.");
-		animal.hunger+=20;
-		animal.happiness-=5;
-		animal.training-=5;
+		console.log(animal.name+" decides to take a nap.\n"+animal.name+" is so stuffed they can barely move! No energy for learning.");
+		animal.changeHunger(10,10);
+		animal.changeTraining(-5,-5);
+		animal.changeHappiness(-1,-5);
 	}
 	else if (animal.happiness<35){
-		console.log(animal.name+" tries really hard, but they seem a bit off.");
-		console.log("Your training only seems somewhat effective. "+animal.name+" needs to have some fun!");
-		animal.hunger+=20;
-		animal.happiness-=10;
-		animal.training+=10;
+		console.log(animal.name+" tries really hard, but they seem a bit off.\nYour training only seems somewhat effective. "+animal.name+" needs to have some fun!");
+		animal.changeHunger(10,10);
+		animal.changeTraining(5,5);
+		animal.changeHappiness(-5,-5);
 	}
 	else{
 		console.log(animal.name+" has learned a new trick! They love showing off.\nHow cute.");
-		animal.hunger+=25;
-		animal.happiness+=10;
-		animal.training+=25;
+		animal.changeHunger(15,10);
+		animal.changeTraining(25,10);
+		animal.changeHappiness(5,5);
 		animal.tricks++
 	}
-	turns++;
+	player.turns++;
 	toContinue();
 }
 
@@ -219,35 +239,34 @@ function showAnimal(){
 	clear();
 	console.log("You sign "+animal.name+" up for tonight's performance...");
 	if (animal.training>=75 && animal.hunger<50 && animal.happiness>2 && animal.tricks>3){
-		console.log(animal.name+" puts on the show of a lifetime! Incredible!");
-		console.log("You are so proud of "+animal.name+" and they are very happy, though exhausted.");
-		animal.hunger+=35;
-		animal.happiness+=50;
-		animal.training+=5;
+		console.log(animal.name+" puts on the show of a lifetime! Incredible!\nYou are so proud of "+animal.name+" and they are very happy, though exhausted.");
+		animal.changeHunger(20,20);
+		animal.changeTraining(5,1);
+		animal.changeHappiness(50,15);
 	}
 	else if (animal.training>=50 && animal.hunger<50 && animal.happiness>2 && animal.tricks>1){
-		console.log(animal.name+" puts on a pretty good show, with only a few mistakes.");
-		console.log("They seem happy but tired.");
-		animal.hunger+=30;
-		animal.happiness+=20;
-		animal.training+=5;
+		console.log(animal.name+" puts on a pretty good show, with only a few mistakes.\nThey seem happy but tired.");
+		animal.changeHunger(20,20);
+		animal.changeTraining(5,1);
+		animal.changeHappiness(15,10);
 	}
 	else if (animal.training>=25 && animal.hunger<50 && animal.happiness>25){
-		console.log(animal.name+" makes several rookie mistakes...");
-		console.log("You never thought a "+animal.species+" could look embarrassed, but you'd be wrong...");
-		console.log("Train harder next time!");
-		animal.hunger+=30;
-		animal.happiness-=5;
-		animal.training+=5;
+		console.log(animal.name+" makes several rookie mistakes...\nYou never thought a "+animal.species+" could look embarrassed, but you'd be wrong...\nTrain harder next time!");
+		animal.changeHunger(20,20);
+		animal.changeTraining(5,1);
+		animal.changeHappiness(-15,-1);
 	}
 	else if (animal.training>-1 && animal.hunger<50 && animal.happiness>25){
-		console.log(animal.name+" looks lost out there. They keep getting in the way!");
-		console.log("How embarrassing! "+animal.name+" is depressed now.");
-		animal.hunger-=30;
-		animal.happiness-=30;
+		console.log(animal.name+" looks lost out there. They keep getting in the way!\nHow embarrassing! "+animal.name+" is depressed now.");
+		animal.changeHunger(20, 20);
+		animal.changeTraining(-1,-1);
+		animal.changeHappiness(-30,-15);
 	}
 	else{
 		console.log(animal.name+" refuses to even enter in the show.\nTake care of their needs first, and train them well.");
+		animal.changeHunger(2,1);
+		animal.changeTraining(-2,-1);
+		animal.changeHappiness(-2,-1);
 		animal.hunger-=10;
 		animal.happiness-=5;
 		animal.training-=5;
@@ -266,24 +285,39 @@ function retire(){
 
 function displayScore(){
 	
-	console.log(animal.name+"'s happiness level is: "+animal.happiness);
-	console.log(animal.name+"'s hunger level is: "+animal.hunger);
-	console.log(animal.name+"'s training level is: "+animal.training);
-	console.log(animal.name+" has learned "+animal.tricks+" tricks.");
+	console.log(animal.name+"'s happiness level is: "+animal.happiness+"\n"+animal.name+"'s hunger level is: "+animal.hunger+"\n"+animal.name+"'s training level is: "+animal.training+"\n"+animal.name+" has learned "+animal.tricks+" tricks.");
 	var grandScore = (animal.happiness+animal.training-animal.hunger)*animal.tricks;
-	console.log("===============================");
-	console.log(player.name.toUpperCase()+"'S GRAND TOTAL SCORE IS: "+grandScore);
-	console.log("THANK YOU FOR PLAYING!");
-	console.log("===============================");
+	console.log("===============================\n"+player.name.toUpperCase()+"'S GRAND TOTAL SCORE IS: "+grandScore+"\nTHANK YOU FOR PLAYING!\n===============================");
 	toContinue();
 }
 
+function displayAnimal(){
+
+	if (animal.happiness>50)
+		console.log(animal.name+" looks happy with its life.");
+	else 
+		console.log(animal.name+" seems mopey.");
+	
+
+	if (animal.hunger<50)
+		console.log(animal.name+" seems full.");
+	else
+		console.log(animal.name+" seems hungry.");
+
+	if (animal.training>50)
+		console.log(animal.name+" seems quite disciplined.");
+	else
+		console.log(animal.name+" seems somewhat wild.");
+
+	console.log(animal.name+" is a "+animal.species+".");
+
+	toContinue();
+	clear();
+
+}
+
 clear();
-console.log("==============================");
-console.log("|    Welcome to GAMMA ZOO    |");
-console.log("|  Home of the Platypus Den  |");
-console.log("|Where YOU are the Zookeeper\u2122|")
-console.log("==============================");
+console.log("==============================\n|    Welcome to GAMMA ZOO    |\n|  Home of the Platypus Den  |\n|Where YOU are the Zookeeper\u2122|\n==============================");
 
 nameAnimal();
 mainMenu();
